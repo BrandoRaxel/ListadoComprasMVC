b108c7a7558a7207eacd02f97b8605ee3a753a7c
Brando Raxel <brando.raxel@gmail.com>
2022-02-21 14:20:01 -0600

    Actualizaciones

---
+++ src/server/Server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {environment} from "../environments/environment.js";
+import Routes from "../routes/Routes.js";
+
+export let WEB_SERVER;
+
+export default class Server {
+    constructor() {
+        this.WebServer();
+    }
+
+    WebServer() {
+        WEB_SERVER = express();
+        this.LoadRoutes();
+        this.InitializePort();
+    }
+
+    LoadRoutes() {
+        const routes = new Routes();
+    }
+
+    InitializePort() {
+        WEB_SERVER.listen(environment.PORT, () => {
+            console.log(`Conectado al puerto ${environment.PORT}`);
+        });
+    }
+}
